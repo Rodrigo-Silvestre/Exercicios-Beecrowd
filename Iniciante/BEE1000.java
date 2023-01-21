eeb33defefabd76372bf24fbdcb75af7f890cc29
Rodrigo_Silvestre <110398744+Rodrigo-Silvestre@users.noreply.github.com>
2023-01-21 14:10:37 -0300

    Update BEE1000.java

--- Iniciante/BEE1000.java
+++ Iniciante/BEE1000.java
@@ -1,4 +1,6 @@
+package Iniciante
 /**
+ * Enunciado:
  * O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!".
  *  Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
  * 

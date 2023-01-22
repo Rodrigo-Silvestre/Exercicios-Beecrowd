ca4ec709bebac648fb98ecef3f3218799bdf12cc
Rodrigo_Silvestre <110398744+Rodrigo-Silvestre@users.noreply.github.com>
2023-01-22 11:20:32 -0300

    Add files via upload

---
+++ Iniciante/BEE1006.java
@@ -0,0 +1,31 @@
+/**
+ * Enunciado:
+ * Leia 3 valores, no caso, variáveis A, B e C, que são as três notas de um aluno.
+ * A seguir, calcule a média do aluno, sabendo que a nota A tem peso 2, a nota B tem peso 3 e a nota C tem peso 5.
+ * Considere que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+ * 
+ * Entrada:
+ * O arquivo de entrada contém 3 valores com uma casa decimal, de dupla precisão (double).
+ * 
+ * Saída:
+ * Imprima a mensagem "MEDIA" e a média do aluno conforme exemplo abaixo,
+ * com 1 dígito após o ponto decimal e com um espaço em branco antes e depois da igualdade.
+ * Assim como todos os problemas, não esqueça de imprimir o fim de linha após o resultado,
+ * caso contrário, você receberá "Presentation Error".
+ */
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner dado = new Scanner(System.in);
+		double notaA, notaB, notaC, media, notaA2, notaB2, notaC2;
+		notaA = dado.nextDouble();
+		notaB = dado.nextDouble();
+		notaC = dado.nextDouble();
+		notaA2 = (notaA * 20) / 100;
+		notaB2 = (notaB * 30) / 100;
+		notaC2 = (notaC * 50) / 100;
+		media = notaA2 + notaB2 + notaC2;
+		System.out.printf("MEDIA = %.1f%n" ,media);
+		dado.close();
+	}
+}
---
+++ Iniciante/BEE1008.java
@@ -0,0 +1,29 @@
+/**
+ * Enunciado:
+ * Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas,
+ * o valor que recebe por hora e calcula o salário desse funcionário.
+ * A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+ * 
+ * Entrada:
+ * O arquivo de entrada contém 2 números inteiros e 1 número com duas casas decimais,representando o número,
+ * quantidade de horas trabalhadas e o valor que o funcionário recebe por hora trabalhada, respectivamente.
+ * 
+ * Saída:
+ * Imprima o número e o salário do funcionário, conforme exemplo fornecido,
+ * com um espaço em branco antes e depois da igualdade. No caso do salário,
+ * também deve haver um espaço em branco após o $
+ */
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner dado = new Scanner(System.in);
+		int numeroFuncionario, quantidadeHoras;
+		double valorHora, salario;
+		numeroFuncionario = dado.nextInt();
+		quantidadeHoras = dado.nextInt();
+		valorHora = dado.nextDouble();
+		salario = quantidadeHoras * valorHora;
+		System.out.printf("NUMBER = %d%nSALARY = U$ %.2f%n", numeroFuncionario, salario);
+		dado.close();
+	}
+}

3882f19d201ec29d9399915501f536a6b671a043
Rodrigo_Silvestre <110398744+Rodrigo-Silvestre@users.noreply.github.com>
2023-01-23 15:38:27 -0300

    Update BEE1018.java

--- Iniciante/BEE1018.java
+++ Iniciante/BEE1018.java
@@ -12,7 +12,7 @@
  */
 import java.io.IOException;
 import java.util.Scanner;
-public class Mai {
+public class Main {
     public static void main(String[] args) throws IOException {
         Scanner dado = new Scanner(System.in);
         int valor;

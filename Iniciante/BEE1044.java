3f74c8b8511c42f8cc1cd191b7fd00755b9464db
Rodrigo_Silvestre <110398744+Rodrigo-Silvestre@users.noreply.github.com>
2023-01-30 18:07:34 -0300

    Create BEE1044.java

---
+++ Iniciante/BEE1044.java
@@ -0,0 +1,40 @@
+/**
+ * Enunciado:
+ * Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos",
+ * indicando se os valores lidos são múltiplos entre si.
+ * 
+ * Entrada:
+ * A entrada contém valores inteiros.
+ * 
+ * Saída:
+ * A saída deve conter uma das mensagens conforme descrito acima.
+ */
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner dado = new Scanner(System.in);
+        int A, B;
+        A = dado.nextInt();
+        B = dado.nextInt();
+        if (A > B) {
+            if ((A % B) == 0) {
+                System.out.println("Sao Multiplos");
+            }
+            else {
+                System.out.println("Nao sao Multiplos");
+            }
+        }
+        else {
+            if (B > A) {
+                if ((B % A) == 0) {
+                    System.out.println("Sao Multiplos");
+                }
+                else {
+                    System.out.println("Nao sao Multiplos");
+                }
+            }
+        }
+        dado.close();
+    }
+}
